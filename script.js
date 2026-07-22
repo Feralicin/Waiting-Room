8f8d8cbf7235a98c14ef1cc24b1452f6b9147d0c
Fera <harropjackmax2@gmail.com>
2026-07-22 10:39:07 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     const Target = new Date(Date.UTC(2032, 9, 1, 11, 21, 2))
     const Text = document.querySelector(".time")
+    const Sound = new Audio("Tick.ogg")
+    Sound.volume = 0.8
+
+    let Old = Text.textContent
     
     function UpdateCountdown() {
         const Now = new Date()
@@ -10,8 +14,15 @@ document.addEventListener("DOMContentLoaded", () => {
         const Hours = Math.floor((Difference % 86400) / 3600)
         const Minutes = Math.floor((Difference % 3600) / 60)
         const Seconds = Difference % 60
-    
-        Text.textContent = `${Days}:${String(Hours).padStart(2, "0")}:${String(Minutes).padStart(2, "0")}:${String(Seconds).padStart(2, "0")}`
+
+        const New = `${Days}:${String(Hours).padStart(2, "0")}:${String(Minutes).padStart(2, "0")}:${String(Seconds).padStart(2, "0")}`
+        Text.textContent = New
+
+        if (Old !== New) {
+            Sound.currentTime = 0
+            Sound.play()
+            Old = New
+        }
     }
     
     UpdateCountdown()
